ca1bf231aab9b0219ca663c07839d36e05f55705
Facundo Reyes <facundotomasreyes@gmail.com>
2022-07-26 18:26:37 +0200

    Removed broadcast

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -11,7 +11,7 @@ const SocketHandler = (req, res) => {
     io.on("connection", (socket) => {
       socket.on("message-sent", (msg) => {
         msg.timestamp = new Date().getTime();
-        socket.broadcast.emit("new-message", msg);
+        socket.emit("new-message", msg);
       });
     });
   }
